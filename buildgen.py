db3049e10d617907fdac4eb70cae5f037b550720
marcs-feh <marcosfehlauer@protonmail.com>
2024-07-27 16:46:30 -0300

    build madness over... for now

--- buildgen.py
+++ buildgen.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python
 
-import os
-import platform
-from os import path
+from os import path, listdir
 from dataclasses import dataclass
+from platform import system as system_name
 
 # -------- Config --------
 cc = 'clang++'
-cflags = ['-O1', '-fPIC']
+cflags = ['-O1', '-fPIC', '-pipe', '-fno-strict-aliasing']
 ldflags = ['-L.']
-packages = ['edit', 'some_utility'] # . is implictly included
+packages = ['core', 'edit']
 exec_name = 'editor'
 
-def pre_build():
-    pass
+output = 'build.ninja'
+# ------- Build ---------
+def main():
+    global packages, cflags, ldflags, exec_name, output
+    packages.append('.')
+    packages = { p: Package(p) for p in packages }
 
-def post_build():
-    pass
-# ------------------------
+    cflags += [f'-I{p.path}' for p in packages.values()]
+    ldflags += [f'-L{p.path}' for p in packages.values()]
+
+    (packages['.']
+        .require(packages['core'])
+        .require(packages['edit'])
+        .use_file('buildgen.py'))
+
+    b_written = 0
+    with open('build.ninja', 'w') as f:
+        b_written += f.write(ninja_header(cc, cflags, ldflags) + '\n')
+        build_steps = ''
+        for _, pkg in packages.items():
+            build_steps += generate_ninja(pkg) + '\n'
+        b_written += f.write(build_steps)
+
+    print(f'Packages: {len(packages)}')
+    print(f'Output File: {output} [Wrote {round(b_written / 1024, 2)}KiB]')
+# ---------------------
 
 def is_header(s: str) -> bool:
     s = s.strip().lower()
@@ -38,12 +57,16 @@ class Package:
     kind : str # 'exec' | 'obj'
     translation_unit : str
 
-    requires : list # List of Package's that are directly used in building
-    uses : list     # Package artifacts that are needed, but not used directly in building
+    requires : list # List of Packages that are directly used in building
+    uses : list     # Package Packages that are needed, but not used directly in building
+
+    file_requires : list # List of filed that are directly used in building
+    file_uses : list     # Package files that are needed, but not used directly in building
+
 
     def require(self, p):
         if self.kind == 'obj':
-            raise Exception(f'Packages of type obj cannot require artifacts')
+            raise Exception(f'Packages of type obj cannot require packages/artifacts')
         self.requires.append(p)
         return self
 
@@ -51,12 +74,21 @@ class Package:
         self.uses.append(p)
         return self
 
+    def use_file(self, path):
+        self.file_uses.append(path)
+        return self
+
+    def require_file(self, path):
+        if self.kind == 'obj':
+            raise Exception(f'Packages of type obj cannot require packages/artifacts')
+        self.file_requires.append(path)
+
     def artifact(self):
         bin_name = path.basename(self.path)
         if self.path == '.':
             bin_name = path.basename(path.abspath(self.path))
 
-        if platform.system() == 'Windows':
+        if system_name() == 'Windows':
             bin_name += '.obj' if self.kind == 'obj' else '.exe'
         else:
             bin_name += '.o' if self.kind == 'obj' else ''
@@ -64,10 +96,13 @@ class Package:
         return bin_name
 
     def __init__(self, root: str):
+        entries = listdir(root)
+        entries = filter(lambda e: is_cpp_source(e) or is_header(e), entries)
+
         self.requires = []
         self.uses = []
-        entries = os.listdir(root)
-        entries = filter(lambda e: is_cpp_source(e) or is_header(e), entries)
+        self.file_requires = []
+        self.file_uses = []
 
         self.path = root
         self.files = list(entries)
@@ -82,73 +117,60 @@ class Package:
         if self.kind != 'obj' and self.kind != 'exec':
             raise Exception(f'Invalid package kind {self.kind}')
 
+
 def lines(*args):
     buf = []
     for a in args:
         buf.append(str(a))
     return '\n'.join(buf)
 
-NINJA_HEADER = lines(
-    f'cxx = {cc}',
-    f'cflags = {" ".join(cflags)}',
-    f'ldflags = {" ".join(ldflags)}\n',
-    'rule compile',
-    '  command = $cxx -c $cflags -o $out $in',
-    'rule build-exe',
-    '  command = $cxx -o $out $in $ldflags\n',
-)
+def ninja_header(cc, cflags, ldflags):
+    return lines(
+        f'cxx = {cc}',
+        f'cflags = {" ".join(cflags)}',
+        f'ldflags = {" ".join(ldflags)}\n',
+        'rule compile',
+        '  command = $cxx -c $cflags -o $out $in',
+        'rule build-exe',
+        '  command = $cxx -o $out $in $ldflags\n',
+    )
 
 def generate_ninja(pkg: Package):
-    lines = []
-    if pkg.kind == 'obj':
-        line = f'build {path.join(pkg.path, pkg.artifact())}: compile {path.join(pkg.path, pkg.translation_unit)}'
+    action = 'compile' if pkg.kind == 'obj' else 'build-exe'
 
-        used_files = [path.join(used.path, used.artifact()) for used in pkg.uses]
+    required_files = [path.join(req.path, req.artifact()) for req in pkg.requires] + pkg.file_requires
+    used_files = [path.join(used.path, used.artifact()) for used in pkg.uses] + pkg.file_uses
 
-        filepaths = [path.join(pkg.path, f) for f in pkg.files]
-        deps = ' '.join(filepaths)
-        line += ' | ' + deps
+    filepaths = [path.join(pkg.path, f) for f in pkg.files]
 
-        lines.append(line)
-    elif pkg.kind == 'exec':
-        line = f'build {path.join(pkg.path, pkg.artifact())}: build-exe {path.join(pkg.path, pkg.translation_unit)}'
+    out = [f'build {path.join(pkg.path, pkg.artifact())}: {action} {path.join(pkg.path, pkg.translation_unit)}']
 
-        req_files = [path.join(req.path, req.artifact()) for req in pkg.requires]
-        used_files = [path.join(used.path, used.artifact()) for used in pkg.uses]
+    if pkg.kind == 'exec':
+        out.append(" ".join(required_files))
 
-        line += ' ' + ' '.join(req_files)
+    out.append('| ' + ' '.join(used_files + filepaths))
 
-        filepaths = [path.join(pkg.path, f) for f in pkg.files]
-        deps = ' '.join(filepaths)
-        line += ' | ' + deps + ' ' + ' '.join(used_files)
+    return ' '.join(out).strip()
 
-        lines.append(line)
-    else:
-        raise Exception(f'Invalid package kind {pkg.kind}')
-
-    return '\n'.join(map(lambda l: l.strip(), lines))
-
-def main():
-    global packages
-    packages.append('.')
-
-    pdict = {}
-    for p in packages:
-        pdict[p] = Package(p)
-    packages = pdict
-
-    print(NINJA_HEADER)
-    for _, pkg in packages.items():
-        print(generate_ninja(pkg))
-
-
-# Note about "Packages", C++ does **not** have real packages, this is a
-# convention I made up for making unity builds easier, a "package" is a
-# directory, it is treated as an executable if it has a `main.cpp` file and as
-# a library if it has a `lib.cpp` file. This is the *only* compiled translation
-# unit for that package. The point of this is to allow for basic modularization
-# and platform specif abstraction without having to buy into some crazy and
-# gargantuan build system, this is a limited approach on purpose. When
-# compiling, the -I and -L flag automatically include the package directory.
 
 if __name__ == '__main__': main()
+
+'''
+This is a simple mini build system to help you generate a build.ninja file from
+a set of directories called "packages".
+
+Note that C++ does **not** have real packages, this is a convention I made up
+for making unity builds easier, a "package" is a directory, it is treated as an
+executable if it has a `main.cpp` file and as a library[1] if it has a `lib.cpp`
+file. This is the *only* compiled translation unit for that package. The point
+of this is to allow for basic modularization and platform specific abstraction
+without having to buy into some crazy and gargantuan build system, this is a
+limited approach on purpose.
+
+Because there's only one translation unit per package, archiving objects is not
+required, so a library is just one object file.
+
+TODO: Dynamic libraries
+
+When compiling, the -I and -L flag automatically include the package directory.
+'''
